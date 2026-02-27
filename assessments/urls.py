d4619b5f020d3b2441a6f157e1500ed04ccb1349
avyuteck-lgtm <avyuteck@gmail.com>
2026-02-27 20:23:35 +0530

    Update urls.py

--- assessments/urls.py
+++ assessments/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import create_assessment, generate_pdf, executive_summary
+from .views import create_assessment, generate_pdf
 
 urlpatterns = [
     path("new/", create_assessment, name="create_assessment"),
     path("report/<int:assessment_id>/", generate_pdf, name="generate_pdf"),
-    path("executive/", executive_summary, name="executive_summary"),
 ]
